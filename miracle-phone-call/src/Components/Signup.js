b5c1abb8e20fa72c7a8931a0401169d365d9439a
aldoortiz128 <aldoortiz128@gmail.com>
2022-05-17 23:38:27 -0400

    Front End updated with React Strap

--- miracle-phone-call/src/Components/Signup.js
+++ miracle-phone-call/src/Components/Signup.js
@@ -2,6 +2,7 @@ import Logo from "./Logo";
 import Footer from "./Footer";
 import {Link} from "react-router-dom";
 import { useState } from "react";
+import { Card, CardBody, CardImg, CardGroup, CardTitle, Button, Form, FormGroup, Label, Input} from 'reactstrap'
 
 export default function Signup() {
   const [username, setUsername] = useState("")
@@ -28,45 +29,148 @@ export default function Signup() {
   }
 
   return(
-    <div className="signup-container">
-      <div className="signup-body">
-        <div className="signup-contents">
-          <Logo />
-          <p>Already have an account, go back to <Link to="/login">Login In</Link></p>
-          <form className="signup-form">
-            <label>Username: </label>
-              <input type="text" onChange = {(event) => {
-                setUsername(event.target.value)
-              }} value = {username}></input>
 
-            <label>First Name (optional): </label>
-            <input type="text" onChange = {(event) => {
+    <div>
+      <CardGroup>
+        <Card body color="light">
+        <h3>Miracle Phone Call</h3>
+        <Form inline>
+        <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+              <Label
+                className="me-sm-2"
+                for="exampleFirstname"
+              >
+              First Name (Optional)
+              </Label>
+              <Input
+                id="exampleFirstname"
+                name="First Name"
+                placeholder="somethingKool"
+                type="text"
+                onChange = {(event) => {
                 setFirstName(event.target.value)
-              }} value = {firstName}></input> 
-            
-            <label>Last Name (optional): </label>
-            <input type="text" onChange = {(event) => {
-                setLastName(event.target.value)
-              }} value = {lastName}></input>
-            
-            <label>Password: </label>
-            <input type="password" onChange = {(event) => {
-                setPassword(event.target.value)
-              }} value = {password}>
-
-            </input>
-            
-            <label>Re-Enter Password: </label>
-              <input type="password" onChange = {(event) => {
-                setRePassword(event.target.value)}} value={repassword}>
-              </input>
-            <br></br>
-            <button onClick = {register}>Create Account</button>
-          </form>
-        </div>
-        <img src="../images/pexels-nicolas-postiglioni-1927155.jpg" className="signup-img"/>
-      </div>
+                }} value = {firstName}
+              />
+            </FormGroup>          
+      
+            <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+              <Label
+                className="me-sm-2"
+                for="exampleLastName"
+              >
+                Last Name (Optional)
+              </Label>
+              <Input
+                id="exampleLastName"
+                name="Last Name"
+                placeholder="somethingKool"
+                type="text"
+                onChange = {(event) => {
+                             setLastName(event.target.value)
+                           }} value = {lastName}
+              />
+            </FormGroup>
+            <FormGroup className="mb-2 me-sm-2 mb-sm-0"> 
+              <Label
+                className="me-sm-2"
+                for="exampleUsername"
+              >
+                Username
+              </Label>
+              <Input
+                id="exampleUsername"
+                name="username"
+                placeholder="somethingKool"
+                type="text"
+                onChange = {(event) => {
+                              setUsername(event.target.value)
+                            }} value = {username}
+              />
+            </FormGroup>
+            <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+              <Label
+                className="me-sm-2"
+                for="examplePassword"
+              >
+                Password
+              </Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="don't tell!"
+                type="password"
+                onChange = {(event) => {
+                              setPassword(event.target.value)
+                            }} value = {password}
+              />
+            </FormGroup>
+            <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+              <Label
+                className="me-sm-2"
+                for="examplePassword"
+              >
+                Re-Enter Password
+              </Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="seriously don't tell!"
+                type="password"
+                onChange = {(event) => {
+                              setRePassword(event.target.value)}} value={repassword}
+              />
+            </FormGroup>
+            <Button color="primary"  onClick = {register}> Register </Button>
+          </Form>
+        </Card>
+        <Card body color="light">
+          <CardImg alt="Card image cap" src="../images/pexels-nicolas-postiglioni-1927155.jpg" top width="100%" />
+        </Card>
+      </CardGroup>
       <Footer />
     </div>
+ 
   )
+
+    // <div className="signup-container">
+    //   <div className="signup-body">
+    //     <div className="signup-contents">
+    //       <Logo />
+    //       <p>Already have an account, go back to <Link to="/login">Login In</Link></p>
+    //       <form className="signup-form">
+    //         <label>Username: </label>
+    //           <input type="text" onChange = {(event) => {
+    //             setUsername(event.target.value)
+    //           }} value = {username}></input>
+
+    //         <label>First Name (optional): </label>
+    //         <input type="text" onChange = {(event) => {
+    //             setFirstName(event.target.value)
+    //           }} value = {firstName}></input> 
+            
+    //         <label>Last Name (optional): </label>
+    //         <input type="text" onChange = {(event) => {
+    //             setLastName(event.target.value)
+    //           }} value = {lastName}></input>
+            
+    //         <label>Password: </label>
+    //         <input type="password" onChange = {(event) => {
+    //             setPassword(event.target.value)
+    //           }} value = {password}>
+
+    //         </input>
+            
+    //         <label>Re-Enter Password: </label>
+    //           <input type="password" onChange = {(event) => {
+    //             setRePassword(event.target.value)}} value={repassword}>
+    //           </input>
+    //         <br></br>
+    //         <button onClick = {register}>Create Account</button>
+    //       </form>
+    //     </div>
+    //     <img src="../images/pexels-nicolas-postiglioni-1927155.jpg" className="signup-img"/>
+    //   </div>
+    //   <Footer />
+    // </div>
+  
 }
